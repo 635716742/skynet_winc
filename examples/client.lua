30a3ffd550b7eb5bc26a4ad643d7b6965c1eb475
陈兵 <635716742@qq.com>
2020-09-06 16:28:57 +0800

    修改工程结构

--- examples/client.lua
+++ examples/client.lua
@@ -1,5 +1,5 @@
 package.cpath = "./luaclib/?.dll;"
-package.path = "../../../lualib/?.lua;../../../examples/?.lua"
+package.path = "../lualib/?.lua;../examples/?.lua"
 
 if _VERSION ~= "Lua 5.3" then
 	error "Use lua 5.3"
